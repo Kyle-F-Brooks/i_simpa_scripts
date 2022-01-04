6e7aec4e3eec3da89fd07ff11a5ee4d61692a635
Kyle-F-Brooks <kylefrankbrooks1997@gmail.com>
2022-01-04 13:09:47 +0000

    added test code for altering project tree

---
+++ UserScript/test_tree/__project_loading__.py
@@ -0,0 +1,5 @@
+import uictrl as ui
+
+rootcore=ui.element(ui.application.getrootcore())
+if rootcore.getelementbylibelle("test_tree")==-1:
+    rootcore.appenduserelement(ui.element_type.ELEMENT_TYPE_CORE_CORE,"test_tree","test_tree")
