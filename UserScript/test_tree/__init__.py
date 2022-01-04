ac24d5c1b8dbbc34fe9806bf3936b4cb6ff14b4c
Kyle-F-Brooks <kylefrankbrooks1997@gmail.com>
2022-01-04 15:22:30 +0000

    Cleaned up code

--- UserScript/test_tree/__init__.py
+++ UserScript/test_tree/__init__.py
@@ -4,27 +4,26 @@ class test_tree(ui.element):
     def __init__(self, idel):
         ui.element.__init__(self, idel)
 
-        if not self.hasproperty("exeName"): #Test if this is a new project initialisation
-            #If this is a new project then we add properties
-            #Add tetgen parameters
+        if not self.hasproperty("exeName"): # Test if this is a new project initialisation
+            # If this is a new project then we add properties
+            # Add tetgen parameters
             self.appendfilsbytype(ui.element_type.ELEMENT_TYPE_CORE_CORE_CONFMAILLAGE)
-            #Add frequencies selection
+            # Add frequencies selection
             self.appendfilsbytype(ui.element_type.ELEMENT_TYPE_CORE_CORE_BFREQSELECTION)
-            #Add configuration core
+            # Add configuration core
             coreconf=ui.element(self.appendfilsbytype(ui.element_type.ELEMENT_TYPE_CORE_CORE_CONFIG))
-            #Append hidden config, used by I-SIMPA to find the core files and binaries
+            # Append hidden config, used by I-SIMPA to find the core files and binaries
             ui.element(self.appendpropertytext("modelName","","mesh.cbin",True,True)).hide()
             ui.element(self.appendpropertytext("tetrameshFileName","","tetramesh.mbin",True,True)).hide()
             ui.element(self.appendpropertytext("exeName","","md.py")).hide()
             ui.element(self.appendpropertytext("corePath","","md\\")).hide()
-            #User options
-            coreconf.appendpropertylist("solver_mode","Calculation mode",[["Time"
-            ,"Static"],[0,1]],0,False,1,True)
+            # User options
+            coreconf.appendpropertylist("solver_mode","Calculation mode",[["Time","Static"],[0,1]],0,False,1,True)
             coreconf.appendpropertybool("with_direct_sound","Use direct sound",True,True)
-            _("Calculation mode")
-            _("Use direct sound")
-            _("Time")
-            _("Static")
+            (u"Calculation mode")
+            (u"Use direct sound")
+            (u"Time")
+            (u"Static")
         else:
             pass #Here in case of loading an existing project
 
