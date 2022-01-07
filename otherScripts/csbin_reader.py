6367ec3166366f50b54a4915732d48ce479cf06d
Kyle-F-Brooks <kylefrankbrooks1997@gmail.com>
2022-01-07 13:51:42 +0000

    push after breaking pc

--- otherScripts/csbin_reader.py
+++ otherScripts/csbin_reader.py
@@ -1,25 +1,36 @@
 # from pathlib import Path
 # print(*Path("C:\Users\Lab Engineer.DESKTOP-HN2JAP3\Documents\i_simpa_scripts\otherScripts").iterdir(), sep="\n")
-
-with open("C:\\Users\\Lab Engineer.DESKTOP-HN2JAP3\\Documents\\i_simpa_scripts\\otherScripts\\Sound level.csbin", mode="rb") as file_in:
-    lines = []
-    for line in file_in:
-        lines.append(line)
-
-del(file_in)
+in_type = input("Is the input a file? (y/n):\n")
 
 
-for line in lines:
-    # x=line.decode('cp1252')
-    print(line)
-    print("\n")
-    
-    # try:
-    #     print(line.decode('cp1252'))
-    # except:
-    #     pass
+# "C:\\Users\\Lab Engineer.DESKTOP-HN2JAP3\\Documents\\i_simpa_scripts\\otherScripts\\Sound level.csbin"
 
+if in_type.lower()=="y":
+    input_data = input("Input file location:\n")
+    try:
+        with open(input_data, mode="rb") as file_in:
+            lines = []
+            for line in file_in:
+                lines.append(line)
+        del(file_in)
 
-
-# s = u'Zåìôèðà'
-# print(s.encode('latin1').decode('cp1251'))
+        for line in lines:
+            # x=line.decode('cp1252')
+            print(line)
+            print("\n")
+            
+            # try:
+            #     print(line.decode('cp1252'))
+            # except:
+            #     pass
+    except:
+        print("Issue occurred")
+if in_type.lower()=='n':
+    input_data = input("Input file location:\n")
+    try:
+        s = input_data
+        print(s.encode('latin1').decode('cp1251'))
+    except:
+        print("Invalid Input")
+else:
+    print("invalid answer")
