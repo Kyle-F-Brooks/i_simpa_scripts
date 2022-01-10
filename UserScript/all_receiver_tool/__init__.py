8e0a69bb4c43aaf7d46b53a2988fcaf526d46333
Kyle-F-Brooks <kylefrankbrooks1997@gmail.com>
2022-01-10 15:20:06 +0000

    altered print statements

--- UserScript/all_receiver_tool/__init__.py
+++ UserScript/all_receiver_tool/__init__.py
@@ -2,7 +2,7 @@
 # Created: 09/12/21
 # Modified:20/12/21
 
-# This tool is used to enable and disable the name tags on each punctual reciever to make viewing of the model easier.
+# This tool is used to enable and disable the name tags on each punctual receiver to make viewing of the model easier.
 # Directivity Lines can now be hidden but not re-enabled.
 import uictrl as ui
 
@@ -16,7 +16,7 @@ def set_direc_display(idgrp):
         ui.element(prop).updatedecimalconfig("v",0) #Y
         ui.element(prop).updatedecimalconfig("w",0) #Z
 
-def set_reciever_activation(idgrp,newstate):
+def set_receiver_activation(idgrp,newstate):
     # depending on input, will show or hide the name label
     grpsrc=ui.element(idgrp)
     all_property=grpsrc.getallelementbytype(ui.element_type.ELEMENT_TYPE_SCENE_RECEPTEURSP_RECEPTEUR_RENDU) 
@@ -26,26 +26,29 @@ def set_reciever_activation(idgrp,newstate):
 class manager:
     def __init__(self):
         # register events for menu buttons 
-        self.enable_reciever_namesid=ui.application.register_event(self.enable_reciever_names)
-        self.disable_reciever_namesid=ui.application.register_event(self.disable_reciever_names)
+        self.enable_receiver_namesid=ui.application.register_event(self.enable_receiver_names)
+        self.disable_receiver_namesid=ui.application.register_event(self.disable_receiver_names)
         self.disable_direc_dispid=ui.application.register_event(self.disable_direc_disp)
 
     def getmenu(self,typeel,idel,menu):
         # create menu buttons and link them to their functions
         submenu=[]
-        submenu.append((u"Enable Names", self.enable_reciever_namesid))
-        submenu.append((u"Disable Names", self.disable_reciever_namesid))
-        menu.insert(2,(u"Reciever Names", submenu))
+        submenu.append((u"Enable Names", self.enable_receiver_namesid))
+        submenu.append((u"Disable Names", self.disable_receiver_namesid))
+        menu.insert(2,(u"Receiver Names", submenu))
         menu.insert(3,(u"Remove Directivity Lines", self.disable_direc_dispid))
         return True
 
-    def enable_reciever_names(self, idgrp):
-        set_reciever_activation(idgrp, True)
+    def enable_receiver_names(self, idgrp):
+        set_receiver_activation(idgrp, True)
+        print("Enabled: Punctual Receiver Names\n")
 
-    def disable_reciever_names(self, idgrp):
-        set_reciever_activation(idgrp, False)
+    def disable_receiver_names(self, idgrp):
+        set_receiver_activation(idgrp, False)
+        print("Disabled: Punctual Receiver Names\n")
         
     def disable_direc_disp(self, idgrp):
         set_direc_display(idgrp)
+        print("Disabled: Directivity Lines\n")
 
 ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE_RECEPTEURSP, manager())
