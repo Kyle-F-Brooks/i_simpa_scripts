c5fc2a153aa4d0e8fc985012c79934dc1ad243c8
Kyle-F-Brooks <kylefrankbrooks1997@gmail.com>
2022-01-20 11:30:35 +0000

    testing dict reading

--- otherScripts/input_test_script.py
+++ otherScripts/input_test_script.py
@@ -1,2 +1,17 @@
-test=input("testintput")
-print("Your answer: "+test)
+# test=input("testintput")
+# print("Your answer: "+test)
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+z=[]
+z.append("a")
+z.append("b")
+x = car.values()
+y=list(x)
+car["year"] = 2018
+
+print(y)
+print(z)
