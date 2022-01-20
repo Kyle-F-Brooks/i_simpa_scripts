acc45818b6e80369776701a1a87b7733f3a447d4
Kyle-F-Brooks <kylefrankbrooks1997@gmail.com>
2022-01-20 11:31:44 +0000

    testing reading gabe files

--- UserScript/test_python/__init__.py
+++ UserScript/test_python/__init__.py
@@ -13,44 +13,18 @@ import os
     # STL = SPLex - 6 - avgSPL + QFF
     # print("Hello")
 
-# def open_as_csv(folderwxid):
-#     folder=ui.element(folderwxid)
-#     for el in folder.childs():
-#         # print(el)
-#         if el[1] == ui.element_type.ELEMENT_TYPE_REPORT_FOLDER:
-#             open_as_csv(el[0])
-#         elif el[1] in [ui.element_type.ELEMENT_TYPE_REPORT_GABE_RECP, ui.element_type.ELEMENT_TYPE_REPORT_GABE, ui.element_type.ELEMENT_TYPE_REPORT_GABE_GAP, ui.element_type.ELEMENT_TYPE_REPORT_GABE_RECPS]:
-#             document = ui.e_file(el[0])
-#             gabedoc=ui.application.getdataarray(document)
-            # for row in gabedoc:
-                # print(row)
-            # path = document.buildfullpath().replace(".gabe",".csv")
-            # with open(path.encode('cp1252'), 'w') as csvfile:
-            #     writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            #     for row in gabedoc:
-            #         writer.writerow(row)
-
-def GetMixedLevel(folderwxid):#, target):
+def testFunction(folderwxid):
     # target refers to the measurement type desired for output
-    cols=[]
     folder=ui.element(folderwxid)
-    recplist=folder.getallelementbytype(ui.element_type.ELEMENT_TYPE_REPORT_GABE_RECP)
-    for idrecp in recplist: # for each punctual receiver in folder "Punctual receivers"
-        recp=ui.element(idrecp)
-        # once the spps is calculated a file named "Sound level" will be in the Project > Results tree
-        if recp.getinfos()["name"]=="fusionSPL":
-            # get the acoustics parameters file    
-            pere=ui.element(recp.getinfos()["parentid"])
-            nomrecp=pere.getinfos()["label"]
-            params=ui.element(pere.getelementbylibelle('Acoustic parameters'))
-            gridparam=ui.application.getdataarray(params)
-            print(gridparam)
-            if len(cols)==0: # with array cols empty 
-            #     cols.append(next(zip(*gridparam))) # add frequency row
-                idcol=gridparam[0].index("200") # set target column equal to the target title
-            # cols.append([nomrecp]+list(list(zip(*gridparam))[idcol][1:])) # add data from reciever to new row
-            print([nomrecp]+list(list(zip(*gridparam))[idcol][1:])) # add data from reciever to new row
-    return cols
+    for el in folder.childs():
+        if el[1] == ui.element_type.ELEMENT_TYPE_REPORT_FOLDER:
+            testFunction(el[0])
+        elif el[1] in [ui.element_type.ELEMENT_TYPE_REPORT_GABE_RECP, ui.element_type.ELEMENT_TYPE_REPORT_GABE, ui.element_type.ELEMENT_TYPE_REPORT_GABE_GAP, ui.element_type.ELEMENT_TYPE_REPORT_GABE_RECPS]:
+            document = ui.e_file(el[0])
+            if document.getinfos()["name"]=="fusionSPL":
+                gridparam=ui.application.getdataarray(document)
+                for row in gridparam:
+                    print(row)
 
 class manager:
     def __init__(self):
@@ -68,7 +42,7 @@ class manager:
     def testFunc(self,idel):
         # transmision_loss_calc()
         input1=u"Please Pick a Receiver"
-        GetMixedLevel(idel)
+        testFunction(idel)
         # res=ui.application.getuserinput((u"This pop-up currently has no function"),(u"Pick a Reciever from the list"),{input1 : "0"})
 
         # open_as_csv(idel)
