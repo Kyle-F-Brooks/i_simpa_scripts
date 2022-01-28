7cdd415deeccedabf8ab1be9aadb4c72cb199e66
Kyle-F-Brooks <kylefrankbrooks1997@gmail.com>
2022-01-28 16:13:40 +0000

    added menu displaying

--- UserScript/plot_xyz/__init__.py
+++ UserScript/plot_xyz/__init__.py
@@ -2,6 +2,21 @@
 # Created: 28/01/2022
 
 from libsimpa import *
-def createMatrix(xVals,yVals,recIds):
+import uictrl as ui
 
+def createMatrix(xVals,yVals,recIds):
+  # recIds is an array of all the receiver IDs
   pass
+
+class manager:
+  def __init__(self):
+    pass
+  def getmenu(self,elementType,elementId,menu):
+    el=ui.element(elementId)
+    infos=el.getinfos()
+    if infos["name"]==u"Punctual receivers": # only display menu on Punctual receivers file
+        menu.insert(0,())
+        menu.insert(0,(u"Plot as XYZ",self.stlCalculationid))
+        return True
+    else:
+      return False
