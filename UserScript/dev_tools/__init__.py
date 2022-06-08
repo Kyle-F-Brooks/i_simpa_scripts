3c8d9fefb4e4787b740851017b0f3a58bdaebf87
Kyle-F-Brooks <kylefrankbrooks1997@gmail.com>
2022-06-08 12:45:37 +0100

    only enabled where needed

--- UserScript/dev_tools/__init__.py
+++ UserScript/dev_tools/__init__.py
@@ -11,16 +11,23 @@ class manager:
         self.showEl=ui.application.register_event(self.showId)
         self.showPaEl=ui.application.register_event(self.showParentId)
     def getmenu(self, elementType, elementId, menu):
-        submenu=[("Show ID",self.showEl),("Show Parent Info",self.showPaEl)]
+        submenu=[("Show Info",self.showEl),("Show Parent Info",self.showPaEl)]
         menu.insert(0,("dev Tools",submenu))
+        return True
     def showId(self,elementId):
-        print(elementId)
+        infos=ui.element(elementId).getinfos()
+        print(infos)
     def showParentId(self,elementId):
         infos=ui.element(elementId).getinfos()
         print(ui.element(infos["parentid"]).getinfos())
 
-ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_REPORT_FOLDER, manager())
-ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE_ROOT,manager())
-ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE_GROUPESURFACES,manager())
-ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE_RECEPTEURSP,manager())
+# ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_REPORT_FOLDER, manager())
+# ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE_ROOT,manager())
+# ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE_GROUPESURFACES,manager())
+# ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE_RECEPTEURSP,manager())
+# ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE_BDD_MATERIAUX, manager())
+ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE_DONNES,manager())
+ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE_PROJET,manager())
+# ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE,manager())
+# ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE,manager())
 # ui.application.register_menu_manager(ui.element_type.ELEMENT_TYPE_SCENE,manager())
